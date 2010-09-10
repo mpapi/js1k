760e97bfba46bd277f39583a7685e3091337fa47
Matt Papi <mmpapi@gmail.com>
2010-09-09 23:05:06 -0400

    more optimization

--- js.js
+++ js.js
@@ -1,38 +1,32 @@
 var canvas = document.getElementById('c');
 var ctx = canvas.getContext('2d');
 var w = 9;
-var h = 9;
-var colors = ['#aaa','#d0d','#dd0','#0dd'];
-var blk = '#000';
+var colors = ['aaa','d0d','dd0','0dd'];
 var M = Math;
-var ww = 500;
-var hh = 500;
-canvas.width = ww;
-canvas.height = ww;
+var ww = canvas.width = canvas.height = 500;
 var pad = 7;
 var slice = ww / w;
 var score = 0;
-function sq() { return colors[M.floor(M.random()*colors.length)]; }
+function sq() { return '#'+colors[M.floor(M.random()*colors.length)]; }
 function loop(fn) {
-  var j = h - 1; while (j--) {
+  var j = w - 1; while (j--) {
     var i = w - 1; while (i--) fn(i, j);
   }
 }
 var squares = {};
 loop(function(i, j) { squares[[i,j]] = sq() });
 function draw() {
-  ctx.clearRect(0, 0, ww, hh);
+  ctx.clearRect(0, 0, ww, ww);
   loop(function(i, j) {
     var color = squares[[i,j]];
-    if (!color) color = blk;
     ctx.fillStyle = color;
     var m = i * slice;
     var n = j * slice;
     ctx.fillRect(m + pad, n + pad, slice - pad, slice - pad);
   });
-  ctx.fillStyle = blk;
+  ctx.fillStyle = '#000';
   ctx.font = 'bold 32px monospace';
-  ctx.fillText(score * 10, ww - 100, 40);
+  ctx.fillText(score * 10, ww - 200, 40);
 }
 var sel = 0;
 var lsel = 0;
@@ -48,7 +42,7 @@ function swap(chk) {
     var d = squares[lsel];
     squares[lsel] = squares[sel];
     squares[sel] = d;
-    if (chk && collapse() == 0) { var d = lsel; lsel = sel; sel = d; swap(0); }
+    if (chk && !collapse()) { var d = lsel; lsel = sel; sel = d; swap(0); }
     else { sel = 0; refresh(); }
   } else {
     sel = lsel;
@@ -66,22 +60,20 @@ function collapse() {
   for (var i = 0; i < matches.length; i++) {
     squares[matches[i]] = 0;
   }
-  while (settle() > 0);
-  draw()
+  do {
+    var s = 0;
+    loop(function(i, j) {
+      if (!squares[[i, j]]) {
+        var d = squares[[i, j - 1]];
+        squares[[i, j]] = d;
+        squares[[i, j - 1]] = 0;
+        if (d) s++;
+      }
+    });
+  }
+  while (s > 0);
   score += matches.length;
   return matches.length;
-}
-function settle() {
-  var count = 0;
-  loop(function(i, j) {
-    if (!squares[[i, j]]) {
-      var d = squares[[i, j - 1]];
-      squares[[i, j]] = d;
-      squares[[i, j - 1]] = 0;
-      if (d) count++;
-    }
-  });
-  return count;
 }
 function fill() {
   var count = 0;
@@ -95,9 +87,9 @@ function fill() {
   return count;
 }
 function refresh() {
-  draw()
   while (collapse() > 0);
   while (fill() > 0);
+  draw()
 }
 refresh();
 score = 0;
